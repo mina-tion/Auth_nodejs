ef9d6e4f4607d854015b5dd08893817561c10df0
Anastasia Skripnik <scripnik.a21@cf.dnu.dp.ua>
2022-06-22 16:23:26 +0300

    add middleware

--- authController.js
+++ authController.js
@@ -1,15 +1,70 @@
+const User = require('./models/User');
+const Role = require('./models/Role');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { validationResult } = require('express-validator');
+const { secret } = require('./config');
+
+const generateAccessToken = (id, roles) => {
+	const payload = {
+		id,
+		roles,
+	};
+	return jwt.sign(payload, secret, { expiresIn: '24h' });
+};
+
 class authController {
 	async registration(req, res) {
 		try {
-		} catch (error) {}
+			const errors = validationResult(req);
+			if (!errors.isEmpty()) {
+				return res.status(400).json('Помилка при реєстрації', errors);
+			}
+			const { username, password } = req.body;
+			const candidate = await User.findOne({ username });
+			if (candidate) {
+				return res
+					.status(400)
+					.json({ message: 'Користувач вже зареєстрований' });
+			}
+			const hashPassword = bcrypt.hashSync(password, 7);
+			const userRole = await Role.findOne({ value: 'USER' });
+			const user = new User({
+				username,
+				password: hashPassword,
+				roles: [userRole.value],
+			});
+			await user.save();
+			return res.json({ message: 'Користувача зареєстровано' });
+		} catch (error) {
+			console.log(error);
+			res.status(400).json({ message: 'Registration error' });
+		}
 	}
 	async login(req, res) {
 		try {
-		} catch (error) {}
+			const { username, password } = req.body;
+			const user = await User.findOne({ username });
+			if (!user) {
+				res.status(400).json({ message: 'Користувача не знайдено' });
+			}
+			const validPassword = bcrypt.compareSync(password, user.password);
+			if (!validPassword) {
+				res.status(400).json({ message: 'Невірний пароль' });
+			}
+			const token = generateAccessToken(user._id, user.roles);
+			return res.json({ token });
+		} catch (error) {
+			console.log(error);
+
+			res.status(400).json({ message: 'Login error' });
+		}
 	}
 	async getUsers(req, res) {
 		try {
-			res.json('server works');
+			const users = await User.find();
+
+			res.json(users);
 		} catch (error) {}
 	}
 }
--- authRouter.js
+++ authRouter.js
@@ -1,8 +1,18 @@
-const Router = require('express')
-const router = new Router()
-const authController = require('./authController')
-router.post('/registration', authController.registration)
-router.post('/login', authController.login)
-router.get('/users', authController.getUsers)
+const Router = require('express');
+const router = new Router();
+const authController = require('./authController');
+const { check } = require('express-validator');
+const authMiddleware =  require('./middleware/authMiddleware')
 
-module.exports = router
+router.post(
+	'/registration',
+	[
+		check('username', 'Пусте поле не допускається').notEmpty(),
+		check('password', 'Від 4х до 12ти символів').isLength({ min: 4, max: 12 }),
+	],
+	authController.registration
+);
+router.post('/login', authController.login);
+router.get('/users', authMiddleware, authController.getUsers);
+
+module.exports = router;
