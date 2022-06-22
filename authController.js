3344474b09711b8cdebac801891323dee7c79a65
Anastasia Skripnik <scripnik.a21@cf.dnu.dp.ua>
2022-06-22 07:16:25 +0300

    add models

---
+++ authController.js
@@ -0,0 +1,17 @@
+class authController {
+	async registration(req, res) {
+		try {
+		} catch (error) {}
+	}
+	async login(req, res) {
+		try {
+		} catch (error) {}
+	}
+	async getUsers(req, res) {
+		try {
+			res.json('server works');
+		} catch (error) {}
+	}
+}
+
+module.exports = new authController();
